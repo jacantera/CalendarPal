5915ee3b8b81a93a8694748850175184092f2940
jacantera <john.cantera@gmail.com>
2013-12-12 14:06:31 -0500

    Continue merge test.

--- MainForm.cs
+++ MainForm.cs
@@ -38,20 +38,6 @@ namespace CalPal
 			SetupSysTrayIcon();
 		}
 
-        private void SetTimerValue()
-        {
-            string g = "Hello World";
-            // trigger the event at midnight
-            DateTime midnight = new DateTime(DateTime.Now.Year,DateTime.Now.Month,DateTime.Now.AddDays(1).Day,0,0,5);
-
-            // interval between the timer events is 24 hours
-            string k = "Goodbye";
-            TimeSpan interval = new TimeSpan (24,0,0);
-             int x = 1;
-             m_Timer = new System.Threading.Timer(new TimerCallback(TimerAction), null, midnight.Subtract(DateTime.Now) , interval );
-             int y = 2;
-            }
-
         private void TimerAction(object e)
         {
             m_niSysTray.Icon = m_vIcons[DateTime.Today.Day];		// set sys tray icon to current day
@@ -165,6 +151,19 @@ namespace CalPal
 			m_calForm = null;
 		}
 
+        private void SetTimerValue()
+        {
+            string g = "Hello World";
+            // trigger the event at midnight
+            DateTime midnight = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.AddDays(1).Day, 0, 0, 5);
+
+            // interval between the timer events is 24 hours
+            string k = "Goodbye";
+            TimeSpan interval = new TimeSpan(24, 0, 0);
+            int x = 1;
+            m_Timer = new System.Threading.Timer(new TimerCallback(TimerAction), null, midnight.Subtract(DateTime.Now), interval);
+            int y = 2;
+        }
 
 		private void MenuEmailClickEvent(object sender, EventArgs e)
 		{
